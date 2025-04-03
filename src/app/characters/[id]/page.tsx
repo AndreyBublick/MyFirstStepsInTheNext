aed29dcd11648748b2051937cafcd339059b4a5a
Andrew <dracon.24041230@mail.ru>
2025-04-03 15:22:04 +0300

    0.0.1

---
+++ src/app/characters/[id]/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+import React from 'react'
+import { Cards } from '@/features/cards/ui/Cards'
+
+const Page = () => {
+  return <Cards />
+}
+export default Page
---
+++ src/features/cards/ui/SingleCardView.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SingleCardView = () => {
+  return (
+    <div>
+      
+      </div>
+  )
+}
+
+export default SingleCardView
