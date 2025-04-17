a860bed7948e79b611e66bd1d891df7fda89507d
Andrew <dracon.24041230@mail.ru>
2025-04-17 22:10:38 +0300

    refactor

--- src/shared/popup/lib/Popup.stories.tsx
+++ src/shared/popup/lib/Popup.stories.tsx
@@ -37,7 +37,6 @@ const meta = {
     layout: 'centered',
   },
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
 
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   /* args: { onClick:  fn() },*/
