c30c882f133199a8f408ae7a374c857893791437
Andrew <dracon.24041230@mail.ru>
2025-04-18 17:52:23 +0300

    refactor

--- src/shared/popup/lib/Popup.stories.tsx
+++ src/shared/popup/lib/Popup.stories.tsx
@@ -38,7 +38,7 @@ const meta = {
   },
   tags: ['autodocs'],
 
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  // Use  to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   /* args: { onClick:  fn() },*/
 } satisfies Meta<typeof Popup>
 
