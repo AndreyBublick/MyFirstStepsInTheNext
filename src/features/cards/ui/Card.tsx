4322a6fa55229386779e9257f6fb09f827b8552a
Andrew <dracon.24041230@mail.ru>
2025-05-19 18:52:44 +0300

    refactor

--- src/features/cards/ui/Card.tsx
+++ src/features/cards/ui/Card.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import { CardBody, type CardBodyProps } from '@/features/cards/ui/CardBody'
-
+/**/
 type Props = {
   id: number
 } & CardBodyProps
