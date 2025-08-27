f71de1f708a866fefa0b34726b4d11c1eafa6254
Andrew <dracon.24041230@mail.ru>
2025-08-27 10:07:24 +0300

    refactor

--- src/app/(pages)/(with Header)/characters/[id]/page.tsx
+++ src/app/(pages)/(with Header)/characters/[id]/page.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { useGetCharacterQuery } from '@/features/cards/lib/api'
 import { SingleCardView } from '@/features/cards/ui/SingleCardView'
-/*11aaвdаsd23*/
+/*11aaв3dаsd23*/
 type Props = {
   params: Promise<{
     id: string
