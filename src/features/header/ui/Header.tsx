6d9a0a30e06570292c63dbe86e679da5ee07ccff
Andrey <dracon.24041230@mail.ru>
2025-10-13 09:03:27 +0300

    refactor

--- src/features/header/ui/Header.tsx
+++ src/features/header/ui/Header.tsx
@@ -4,7 +4,7 @@ import s from './Header.module.scss'
 import Link from 'next/link'
 
 type Props = ComponentPropsWithoutRef<'header'>
-/*рHфgthXgGgл*/
+/*рHфgthgGgл*/
 export const Header = ({ children, ...rest }: Props) => {
   return (
     <>
